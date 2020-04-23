c677511859e6e72939b14b5bacf67c2404cb8692
Arsenii Baskakov <murmans1996@gmail.com>
2020-04-23 23:18:06 +0300

    Shopping Cart rewrited

--- cart.js
+++ cart.js
@@ -1,42 +1,53 @@
 'use strict'
 
-//let cartBlock = $(".cart");
-
-
-
-
 
 class ShoppingCart{
 
     constructor(){
         this.cartBlock = $(".cart");
         this.cartBlockItems = this.cartBlock.children(".cart-items-to-buy");
-        this.orderedItems = [];
+        this.orderedItems = new Map();
     }
 
     shopCartLengthCheck(){
-        if(this.orderedItems.length == 0) 
+        if(this.orderedItems.size  == 0) 
             $(".cart").addClass('d-none');
         else
             $(".cart").removeClass('d-none');
         for(let i of this.orderedItems)
-            console.log(i)
+            console.log(i);
 
     }
 
-    addItem(item){
-        // if(this.orderedItems.includes(item)){
-        if(this.orderedItems.find((i, index, arr) => i.id == item.id ) ){
-            let inputElement = this.cartBlockItems.find("#"+item.id).find("input");
-            inputElement.val(Number(inputElement.val()) + 1);
+    updCartDOM(item){
+        if(item.count <= 0){
+            this.cartBlockItems.find("#"+item.id).remove();
+            this.orderedItems.delete(item.id);
         }
         else{
-            this.orderedItems.push(item);
+            let inputElement = this.cartBlockItems.find("#"+item.id).find("input");
+            console.log("3###");
+            console.log(inputElement.val());
+            inputElement.val(item.count);
+
+            console.log(item.count);
+            console.log("3###");
+        }
+    }
+
+    addItem(item){
+        if(this.orderedItems.has(item.id)){
+            this.orderedItems.get(item.id).count++;
+            console.log(this.orderedItems.get(item.id).count + " - cur count");
+            this.updCartDOM(this.orderedItems.get(item.id));
+            console.log("!!!!this.orderedItems.has(item.id)")
+        }
+        else{
+            this.orderedItems.set(item.id, item);
             let newGood = $('<div class = "row text-center no-gutters pt-3 pb-3 " id='+item.id+'><div>');
             $('<div class="col col-sm-3"></div>').append('<span></span>').text(item.name).appendTo(newGood);
             $('<div class="col col-sm-3"></div>').append('<span></span>').text(item.price).appendTo(newGood);
-            // newGood.append(creatPlusMinusInput(item));
-            newGood.append(this.itemToDOM(item));
+            newGood.append(this.itemToDOM(item.id));
             this.cartBlockItems.append(newGood);
         }
 
@@ -44,55 +55,38 @@ class ShoppingCart{
 
     }
     removeItem(item){
-        if(this.orderedItems.find((i, index, arr) => i.id == item.id )){
-            let inputElement = this.cartBlockItems.find("#"+item.id).find("input");
-            if(Number(inputElement.val()) - 1 <= 0){
-              this.cartBlockItems.find("#"+item.id).remove();
-              this.orderedItems.find((i, index, arr) =>{ if(i.id == item.id) arr.splice(1, index)});
-            }
-            else
-                inputElement.val(Number(inputElement.val()) - 1);
+        if(this.orderedItems.has(item.id)){
+            this.orderedItems.get(item.id).count--;
+            this.updCartDOM(this.orderedItems.get(item.id));
             this.shopCartLengthCheck();
         }
     }
 
-    itemToDOM(item){
-        let htmlDescript = '<div class=" col col-sm-6 d-flex plus-minus-input">'
-        +'<button type="button" class="simp_btn btn-remove-item" >'
-          +'<i class="fas fa-minus fa-xs"></i>'
-        +'</button>'
-        +'<input type="number" value="1">'
-        +'<button type="button" class="simp_btn bttn-add-item" >'
-          +'<i class="fas fa-plus fa-xs"></i>'
-        +'</button>'
-      +'</div>';
-    
-        // let parentBlock = $.parseHTML('<div class=" col col-sm-6 d-flex plus-minus-input"></div>');
+    itemToDOM(itemId){
+        let item = this.orderedItems.get(itemId);
         let parentBlock = $("<div></div>").addClass('col col-sm-6 d-flex plus-minus-input');
     
-        // let minusBttn = $.parseHTML('<button type="button" class="simp_btn btn-remove-item" >'
-        //                             +'<i class="fas fa-minus fa-xs"></i>'
-        //                             +'</button>');
-        // console.log(minusBttn);
+
         let minusBttn = $('<button type="button" class="simp_btn btn-remove-item"></button>');
         minusBttn.append($('<i class="fas fa-minus fa-xs"></i>'));
-        minusBttn.on("click", event => shopCart.removeItem(item));
+        minusBttn.on("click", event => this.removeItem(item));
         parentBlock.append(minusBttn);
         
         let inputElement = $('<input type="number" value="1">');
-        // inputElement.on("input", event => if(event.target.value =))
-        // parentBlock.append($.parseHTML('<input type="number" value="1">'));
+        inputElement.on("input", event => { item.count = event.target.value; 
+                                            console.log(item.count + " cur item count in input event!")
+                                            this.updCartDOM(item);})
         parentBlock.append(inputElement);
     
         let plusBttn = $('<button type="button" class="simp_btn btn-add-item"></button>');
         plusBttn.append($('<i class="fas fa-plus fa-xs"></i>'));
-        plusBttn.on("click", event => shopCart.addItem(item));
+        plusBttn.on("click", event => this.addItem(item));
         parentBlock.append(plusBttn);
-    //   return $.parseHTML(htmlDescript);
       return parentBlock;       
     }
 
 }
+
 
 let shopCart;
 shopCart = new ShoppingCart();
@@ -101,7 +95,6 @@ let orderedItems = [];
 let addButtons = $(".bttn-add-item");
 
 addButtons.on("click", function(event){
-    //let curItem = new Item($(event.currentTarget.parentNode));
     let nodeData = Item.extractDataFromNode($(event.currentTarget.parentNode));
     let curItem  = new Item(nodeData);
     shopCart.addItem(curItem);
@@ -122,6 +115,7 @@ class Item{
         this.name = node.name;
         this.price = node.price;
         this.id = node.id;
+        this.count = 1;
     }
     alertData(){
         console.log(this.name);
@@ -129,40 +123,6 @@ class Item{
     }
 
 }
-
-
-// function creatPlusMinusInput(item){
-//     let htmlDescript = '<div class=" col col-sm-6 d-flex plus-minus-input">'
-//     +'<button type="button" class="simp_btn btn-remove-item" >'
-//       +'<i class="fas fa-minus fa-xs"></i>'
-//     +'</button>'
-//     +'<input type="number" value="1">'
-//     +'<button type="button" class="simp_btn bttn-add-item" >'
-//       +'<i class="fas fa-plus fa-xs"></i>'
-//     +'</button>'
-//   +'</div>';
-
-//     // let parentBlock = $.parseHTML('<div class=" col col-sm-6 d-flex plus-minus-input"></div>');
-//     let parentBlock = $("<div></div>").addClass('col col-sm-6 d-flex plus-minus-input');
-
-//     // let minusBttn = $.parseHTML('<button type="button" class="simp_btn btn-remove-item" >'
-//     //                             +'<i class="fas fa-minus fa-xs"></i>'
-//     //                             +'</button>');
-//     // console.log(minusBttn);
-//     let minusBttn = $('<button type="button" class="simp_btn btn-remove-item"></button>');
-//     minusBttn.append($('<i class="fas fa-minus fa-xs"></i>'));
-//     minusBttn.on("click", event => shopCart.removeItem(item));
-//     parentBlock.append(minusBttn);
-
-//     parentBlock.append($.parseHTML('<input type="number" value="1">'));
-
-//     let plusBttn = $('<button type="button" class="simp_btn btn-add-item"></button>');
-//     plusBttn.append($('<i class="fas fa-plus fa-xs"></i>'));
-//     plusBttn.on("click", event => shopCart.addItem(item));
-//     parentBlock.append(plusBttn);
-// //   return $.parseHTML(htmlDescript);
-//   return parentBlock;
-// }
 
 
 
